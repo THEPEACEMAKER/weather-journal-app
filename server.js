8cd862261c4a69beacc73287bb65d4c832277bbc
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2025-01-10 03:33:17 +0200

    Implement first GET and POST routes for managing project data.

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = { good: "great" };
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
+// GET route to send projectData
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
+
+// POST route to add data to projectData
+app.post("/add", (req, res) => {
+  const { temperature, date, userResponse } = req.body;
+  projectData = {
+    temperature: temperature,
+    date: date,
+    userResponse: userResponse,
+  };
+  res.send({ message: "Data added successfully!" });
+});
 
 // Setup Server
 const port = 3000;
 const server = app.listen(port, () => {
-    console.log(`Server running on localhost:${port}`);
+  console.log(`Server running on localhost:${port}`);
 });
